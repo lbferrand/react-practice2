ddfcfb1971419bc51bc0765651a6d43b811f281c
lbferrand <lbferrand@gmail.com>
2022-04-21 10:36:30 -0400

    Create component using useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [year, setYear] = useState(2050);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>{year}</h2>
+          <button onClick={() => setYear(year + 1)}>New Year</button>
+        </div>
+        <div>
+          <h1>Manager on Duty: {manager}</h1>
+          <button onClick={() => setManager("Rachel")}>New Manager</button>
+        </div>
+        <div>
+          <h1>Status: {status}</h1>
+          <button onClick={() => setStatus("Open")}>Open</button>
+          <button onClick={() => setStatus("Back in 5")}>Break</button>
+          <button onClick={() => setStatus("Closed")}>Closed</button>
+        </div>
       </header>
     </div>
   );
